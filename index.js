93fd03327b03e0d3901a1cad2bdb9c38b2c42017
Smith Ringtho <sringtho@gmail.com>
2022-12-27 09:21:52 +0300

    Added copy color code to clipboard functionality

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 
-document.getElementById('submit-btn').addEventListener('click', function(){
+document.addEventListener('click', function(e){
+    if (e.target.id === "submit-btn"){
+        handleSubmitBtn()
+    }
+
+    if (e.target.dataset.code){
+        handleCopyTextToClipBoard(e.target.dataset.code)
+    }
+})
+
+function handleSubmitBtn(){
     const colorHexCode = document.getElementById('color-picker').value.slice(1)
     const schemeMode = document.getElementById('scheme-mode').value
 
@@ -7,20 +17,27 @@ document.getElementById('submit-btn').addEventListener('click', function(){
         .then(res => res.json())
         .then(data => {
             let html = ""
-            data.colors.forEach(color => {
+            console.log(data.colors)
+            data.colors.forEach((color, index) => {
                 html += `
                 <section class="main-color">
                     <div class="color" style="background-color:${color.hex.value};"></div>
                     <div class="color-code">
-                        <p>${color.hex.value}</p>
+                        <p data-code="color-${index+1}" id="color-${index+1}">${color.hex.value}</p>
                     </div>
                 </section>
                 `
             });
             document.getElementById('main-container').innerHTML = html  
         })
-})
+}
 
-
-
+function handleCopyTextToClipBoard(id){
+    let text = document.getElementById(id).textContent;
+    navigator.clipboard.writeText(text).then(() => {
+        alert("Copied the text: " + text)
+      },() => {
+        console.error('Failed to copy');
+    });
+}
 
